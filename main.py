fc7f66fdd2716d9e7119c1dbdfbdd48d0ac76bbb
Sleep zippy <ala.za@obmontessori.edu.ph>
2026-02-27 07:17:57 +0800

    Add players 'Stuart', 'Kevin', and 'Jimenez' to showcase

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pyscript import document, display
 
 def showcase(a):
     document.getElementById("show").innerHTML = "" #clears the output
-    players = ['Agena', 'Ala', 'Baylon','Baylon', 'Brodhagen', 'Cabatingan', 'Canete', 'Dimaculangan', 'Evangelista', 'Galang', 'Garabiles', 'Gonzales', 'Jamet', 'Ledesma', 'Nacino', 'Nardo', 'Oliveros', 'Olmedo', 'Ong', 'Rebadulla', 'Reyes', 'Sangreo', 'Villafuerte', 'Villegas', 'Yao', 'Bob']
+    players = ['Agena', 'Ala', 'Baylon','Baylon', 'Brodhagen', 'Cabatingan', 'Canete', 'Dimaculangan', 'Evangelista', 'Galang', 'Garabiles', 'Gonzales', 'Jamet', 'Ledesma', 'Nacino', 'Nardo', 'Oliveros', 'Olmedo', 'Ong', 'Rebadulla', 'Reyes', 'Sangreo', 'Villafuerte', 'Villegas', 'Yao', 'Bob', 'Stuart', 'Kevin', 'Jimenez']
     for player in players:
+
             display(player, target = "show") #showcases the players
