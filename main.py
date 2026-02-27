518dbafcb0a3c729c160c090933fcb34580d3333
freii ☘︎ <jamet.fa@obmontessori.edu.ph>
2026-02-27 08:38:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from pyscript import document, display
 
 def showcase(a):
     document.getElementById("show").innerHTML = "" #clears the output
-    players = ['Agena', 'Ala', 'Baylon','Baylon', 'Brodhagen', 'Cabatingan', 'Canete', 'Dimaculangan', 'Evangelista', 'Galang', 'Garabiles', 'Gonzales', 'Jamet', 'Ledesma', 'Nacino', 'Nardo', 'Oliveros', 'Olmedo', 'Ong', 'er*nmy alpha', 'Reyes', 'Sangreo', 'Villafuerte', 'Villegas', 'Yao', 'Bob', 'Stuart', 'Kevin', 'Jimenez']
+    players = ['Agena', 'Ala', 'Baylon','Baylon', 'Brodhagen', 'Cabatingan', 'Canete', 'Dimaculangan', 'Evangelista', 'Galang', 'Garabiles', 'Gonzales', 'Jamet', 'Ledesma', 'Nacino', 'Nardo', 'Oliveros', 'Olmedo', 'Ong', 'Rebadulla the auramaxxer', 'Reyes', 'Sangreo', 'Villafuerte', 'Villegas', 'Yao', 'Bob', 'Stuart', 'Kevin', 'Jimenez']
     for player in players:
 
             display(player, target = "show") #showcases the players
+
 
